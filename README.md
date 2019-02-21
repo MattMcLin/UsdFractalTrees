6e897f89b7c95b611e199b3fda27233fb69008c9
Matt McLin <m.r.mclin@gmail.com>
2019-02-21 01:15:58 -0800

    Link in USD libraries, attempt to create a USD stage.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # UsdFractalTrees
 Generate USD geometry for a fractal-based trees
+
+### WARNING
+This is a work in progress. 
+
+## Dependencies
+- cmake 3.0 or later
+- Python 2.7 64-bit
+- Pixar USD source
+- a compiler such as those provided with Visual Studio 2015 or Xcode
+- `pip install pyproj`
+
+## How to build
+### Windows
+git clone https://github.com/MattMcLin/UsdFractalTrees.git
+mkdir build
+cd build
+cmake .. -G "Visual Studio 14 2015 Win64"
+cmake --build .
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,23 @@
 #include "Version.h"
 #include <stdio.h>
 
+#include "pxr/usd/usd/stage.h"
+
 int main(int argc, char* argv[])
 {
 	printf("UsdFractalTrees v%d.%d.%d\n",
 		UsdFractalTrees_VERSION_MAJOR,
 		UsdFractalTrees_VERSION_MINOR,
 		UsdFractalTrees_VERSION_PATCH);
+
+    const std::string outFileName("test.usda");
+    pxr::UsdStageRefPtr stage = pxr::UsdStage::CreateNew(outFileName);
+    if (stage) {
+        stage->GetRootLayer()->Save();
+        printf("Success!\n");
+    } else {
+        printf("Failed to create stage.\n");
+    }
 		
 	return 0;
 }
