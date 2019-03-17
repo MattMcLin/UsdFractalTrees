54b6b55801686a263df08d50a30f64aa882d46d6
Matt McLin <mmclin@apple.com>
2019-03-17 08:42:15 -0700

    Fixes for building on Mac, and first pass at tree generation.

---
+++ src/branch.cpp
@@ -0,0 +1,8 @@
+//
+//  branch.cpp
+//  UsdFractalTrees
+//
+//  Created by Matthew McLin on 3/16/19.
+//
+
+#include "branch.hpp"
---
+++ src/branch.hpp
@@ -0,0 +1,13 @@
+//
+//  branch.hpp
+//  UsdFractalTrees
+//
+//  Created by Matthew McLin on 3/16/19.
+//
+
+#ifndef branch_hpp
+#define branch_hpp
+
+#include <stdio.h>
+
+#endif /* branch_hpp */
